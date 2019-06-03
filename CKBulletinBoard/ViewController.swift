a4ef65911dcd750f8749b28fb68eb987c0175d10
Jordan Hendrickson <jordanhendrickson@Jordans-MacBook-Pro.local>
2019-06-03 10:38:13 -0600

    first commit

---
+++ CKBulletinBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CKBulletinBoard
+//
+//  Created by Jordan Hendrickson on 6/3/19.
+//  Copyright © 2019 Jordan Hendrickson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
